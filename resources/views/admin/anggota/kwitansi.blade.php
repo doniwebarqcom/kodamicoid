6e60aec7925a68b51181acbe1a5b688e81df36a1
Ramdoni <doni.enginer@gmail.com>
2018-08-24 20:41:09 +0700

    Update

--- resources/views/admin/anggota/kwitansi.blade.php
+++ resources/views/admin/anggota/kwitansi.blade.php
@@ -10,8 +10,8 @@
     <section class="sheet padding-5mm">
         <article>
             <p class="img"><img src="{{ asset('logo-biru.png')}}" /></p>
-            <p class="text-center">KWITANSI PEMBAYARAN</p>
             <hr />
+            <p class="text-center">KWITANSI PEMBAYARAN {{ strtoupper(type_deposit($data->type)) }}</p>
             <table class="table">
                 <thead>
                     <tr>
@@ -20,13 +20,22 @@
                     </tr>
                     <tr>
                         <td>TANGGAL</td>
-                        <td> : {{ date('d F Y') }}</td>
+                        <td> : {{ date('d F Y H:i:s') }}</td>
                     </tr>
                         <td>NOMINAL</td>
                         <td> : {{ number_format($data->nominal) }}</td>
                     </tr>
+                    <tr>
+                        <td>STATUS</td>
+                        <td> : BERHASIL</td>
+                    </tr>
                 </thead>
             </table>
+            <p class="text-center">
+                SIMPAN KWITANSI INI SEBAGAI BUKTI TRANSAKSI TERIMAKASIH
+            </p>
+            <hr />
+            <p>Koperasi Produsen Daya Masyarakat Indonesia</p>
         </article>
     </section>
 </body>
