3c2e8c884199afa77bfbea823c2f82864f182208
Ramdoni <doni.enginer@gmail.com>
2018-09-05 09:27:48 +0700

    Up

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -18,7 +18,7 @@
             <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
 
                 <ol class="breadcrumb">
-                    <li><a href="javascript:void(0)">Dashboard => </a></li>
+                    <li><a href="javascript:void(0)">Dashboard </a></li>
                     <li class="active">Home</li>
                 </ol>
             </div>
