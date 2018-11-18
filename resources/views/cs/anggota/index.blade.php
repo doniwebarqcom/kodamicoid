a81f4c0a30d8c6ce3f2135a81423e89a8bc5d0e2
Ramdoni <doni.enginer@gmail.com>
2018-11-18 17:14:45 +0700

    Update

--- resources/views/cs/anggota/index.blade.php
+++ resources/views/cs/anggota/index.blade.php
@@ -14,7 +14,7 @@
     <div class="container-fluid">
         <div class="row bg-title">
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h4 class="page-title">Dashboard</h4> </div>
+                <h4 class="page-title">Manage Anggota</h4> </div>
             <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                 <a href="{{ route('cs.anggota.create') }}" class="btn btn-success btn-sm pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light"> <i class="fa fa-plus"></i> TAMBAH ANGGOTA</a>
                 <ol class="breadcrumb">
@@ -28,8 +28,6 @@
         <div class="row">
             <div class="col-md-12">
                <div class="white-box">
-                    <h3 class="box-title m-b-0">Manage Anggota</h3>
-                    <br />
                     <div class="table-responsive">
                         <table class="display nowrap" id="data_table_no_button" cellspacing="0" width="100%">
                             <thead>
